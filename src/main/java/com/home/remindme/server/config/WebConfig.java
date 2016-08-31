00a1296fc65edfe6df4c3b2b38b2de7d6e177b31
vlasevich <vlasentiy@gmail.com>
2016-08-31 11:04:38 +0300

    Configured base project

---
+++ src/main/java/com/home/remindme/server/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.home.remindme.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.home.remindme.server")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
