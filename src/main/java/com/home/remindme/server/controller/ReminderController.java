67ea017c58244e4046c66d328a2de3123eb98227
vlasevich <vlasentiy@gmail.com>
2016-08-31 12:30:04 +0300

    created REST and return first json from server

--- src/main/java/com/home/remindme/server/controller/ReminderController.java
+++ src/main/java/com/home/remindme/server/controller/ReminderController.java
@@ -1,18 +1,30 @@
 package com.home.remindme.server.controller;
 
-import org.springframework.stereotype.Controller;
+import com.home.remindme.server.entity.Remind;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Date;
+
+@RestController
 @RequestMapping("/remind")
 public class ReminderController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getReminder(ModelMap modelMap) {
-        return "hi";
+    public Remind getReminder(ModelMap modelMap) {
+        return createMockRemind();
+    }
+
+    private Remind createMockRemind() {
+        Remind r = new Remind();
+        r.setId(1);
+        r.setRemindDate(new Date());
+        r.setTitle("my First remind");
+
+        return r;
     }
 }
