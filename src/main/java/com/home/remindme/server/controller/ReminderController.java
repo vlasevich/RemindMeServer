ddbb1eeb1da137c9ad167b757aedc4441d10d218
vlasevich <vlasentiy@gmail.com>
2016-09-02 12:00:36 +0300

    fixed bug with "Could not open JPA EntityManager for transaction" error - changed hibernate version

--- src/main/java/com/home/remindme/server/controller/ReminderController.java
+++ src/main/java/com/home/remindme/server/controller/ReminderController.java
@@ -1,21 +1,33 @@
 package com.home.remindme.server.controller;
 
+
 import com.home.remindme.server.entity.Remind;
-import org.springframework.ui.ModelMap;
+import com.home.remindme.server.repository.RemindRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/remind")
 public class ReminderController {
 
+    final
+    RemindRepository remindRepository;
+
+    @Autowired
+    public ReminderController(RemindRepository remindRepository) {
+        this.remindRepository = remindRepository;
+    }
+
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public Remind getReminder(ModelMap modelMap) {
+    public Remind getReminder() {
+        List<Remind> remindList = remindRepository.findAll();
         return createMockRemind();
     }
 
